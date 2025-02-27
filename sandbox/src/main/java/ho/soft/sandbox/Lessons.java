eca21b7928e6f40eb7fa997c0435f5ccfdd487ff
sholize <59121284+sholize@users.noreply.github.com>
2025-02-27 19:31:54 +0300

    sandbox
    создан проект
    добавлена структура

---
+++ sandbox/src/main/java/ho/soft/sandbox/Lessons.java
@@ -0,0 +1,9 @@
+package ho.soft.sandbox;
+
+public class Lessons {
+
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "softwareT"
+include("sandbox")
