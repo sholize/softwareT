9691767dc5372c7d1baa2135fe9ddf7867114958
sholize <59121284+sholize@users.noreply.github.com>
2025-03-31 13:38:54 +0300

    добавлены функции

--- sandbox/src/main/java/ho/soft/sandbox/Lessons.java
+++ sandbox/src/main/java/ho/soft/sandbox/Lessons.java
@@ -3,7 +3,28 @@ package ho.soft.sandbox;
 public class Lessons {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    hello("Vasia");
+    double l = 5;
+
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a =4;
+    double b = 5;
+    System.out.println("Площадь прямогульника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len){
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
   }
 
 }
